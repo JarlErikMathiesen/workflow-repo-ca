0b387f60b5572125cbc76818849198ea81b8602f
JarlErikMathiesen <117684143+JarlErikMathiesen@users.noreply.github.com>
2025-02-09 16:14:19 +0100

    lasts commit problems

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Workflow repo for the CA
+
+This repository features a venue browsing app with testing functonality. It's for a school project.
+
+## Prerequisites
+
+Make sure you have the following installed before beginning
+
+*Node.js
+*npm
--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -4,13 +4,10 @@ test('when the first venue is clicked from the home page it should display venue
   page,
 }) => {
   // Go to the home page
-  await page.goto('/index.html');
+  await page.goto('/');
 
-  // Wait for the new page to load and check its content
-  await expect(page.locator('#venue-container > a:nth-child(1)')).toBeVisible();
-
-  // Click on the first venue page
-  await page.click('#venue-container > a:nth-child(1)');
+  // Click on the first venue link
+  await page.locator('#venue-container > a:nth-child(1)').click();
 
   // Verify the venue details page has the correct heading
   await expect(page.locator('h1')).toContainText('Venue details');
